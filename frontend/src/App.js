f49a7c3fe3389081ed578c21cf5a2db6662d6c62
bushbass <alex.nielsen@gmail.com>
2023-06-30 12:20:44 -0400

    finished lesson 9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 // pages & components
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Navbar />
-        <div className="pages">
+        <div className='pages'>
           <Routes>
-            <Route 
-              path="/"
-              element={<Home />}
-            />
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
           </Routes>
         </div>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-
   return (
     <header>
-      <div className="container">
-        <Link to="/">
+      <div className='container'>
+        <Link to='/'>
           <h1>Workout Buddy</h1>
         </Link>
+        <nav>
+          <Link to='/login'>Login</Link>
+          <Link to='/signup'>Signup</Link>
+        </nav>
       </div>
     </header>
   )
