5187c98c6b53410fe9c8ede357bd491fe76a1812
bushbass <alex.nielsen@gmail.com>
2023-06-30 17:43:06 -0400

    finished 13

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { login, error, isLoading } = useLogin()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(email, password)
+    await login(email, password)
   }
 
   return (
@@ -24,7 +26,8 @@ function Login() {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button>Submit</button>
+      <button disabled={isLoading}>Submit</button>
+      {error && <div className='error'>{error}</div>}
     </form>
   )
 }
